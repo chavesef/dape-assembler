1dc1709c54cc8b45ca33ed75460ebf83cdc72071
chavesef <echaves1@uolinc.com>
2025-05-17 20:26:38 -0300

    feat: configurações iniciais do S3 e property utils

---
+++ src/main/java/com/dape/assembler/DapeAssemblerApplication.java
@@ -0,0 +1,18 @@
+package com.dape.assembler;
+
+import com.dape.assembler.config.S3Config;
+import com.dape.assembler.util.PropertyUtils;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapeAssemblerApplication {
+
+	public static void main(String[] args) {
+		new DapeAssemblerApplication().run(args);
+	}
+
+	public void run(String[] args) {
+		final PropertyUtils propertyUtils = PropertyUtils.getInstance();
+		final S3Config s3Config = new S3Config(propertyUtils);
+	}
+}
